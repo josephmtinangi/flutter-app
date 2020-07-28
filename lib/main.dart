92cce962a6e95a094142de8aae0f95b6ceefc63d
Joseph Mtinangi <josephmtinangi@gmail.com>
2020-07-28 12:23:27 +0300

    Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() => runApp(MyHomePage());
 
@@ -10,60 +8,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-  String _title = 'My todo';
-  String _subtitle = 'Subtitle';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('API Request'),
+          title: Text('Gradient'),
         ),
-        body: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text(_title),
-              subtitle: Text('completed: $_subtitle'),
-            ),
-            RaisedButton(
-              child: Text(
-                'Get Data',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                makeRequest();
-              },
-              color: Colors.red,
-            )
-          ],
+        body: Center(
+          child: Container(
+            height: 120.0,
+            width: 200.0,
+            decoration: BoxDecoration(
+                gradient: RadialGradient(
+                    colors: [Colors.lightBlue, Colors.blue],
+                    center: Alignment.center,
+                    radius: 1.0)),
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
     );
-  }
-
-  void makeRequest() async {
-    String _url = 'https://jsonplaceholder.typicode.com/todos';
-    print('url');
-    print(_url);
-
-    var response = await http.get(_url);
-    print('response');
-    print(response);
-    String dataAsString = response.body;
-    print('dataAsString');
-    print(dataAsString);
-    List decodedData = json.decode(dataAsString);
-    print('decodedData');
-    print(decodedData);
-
-    Map<String, dynamic> firstToDo = decodedData[0];
-
-    setState(() {
-      _title = firstToDo['title'];
-      _subtitle = firstToDo['completed'].toString();
-    });
   }
 }
