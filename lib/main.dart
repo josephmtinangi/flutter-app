3036b954fc0ca5eb4fe8a0a8b47d4c12321b312e
Joseph Mtinangi <josephmtinangi@gmail.com>
2020-07-28 13:12:26 +0300

    Future builder

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
+  // hold the future
+  Future<bool> completed;
+
+  // hold the data
+  String _receivedData;
+
+  @override
+  void initState() {
+    print('initState');
+
+    super.initState();
+
+    completed = loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter',
-      home: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('My TabBar'),
-              bottom: TabBar(tabs: <Widget>[
-                Tab(text: 'Yellow'),
-                Tab(text: 'Orange'),
-                Tab(text: 'Red')
-              ]),
-            ),
-            body: TabBarView(children: <Widget>[
-              Container(color: Colors.yellow),
-              Container(color: Colors.orange),
-              Container(color: Colors.red)
-            ]),
-          )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Future Builder'),
+        ),
+        body: Center(
+            child: FutureBuilder(
+                future: completed,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(_receivedData);
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                })),
+      ),
       debugShowCheckedModeBanner: false,
     );
+  }
+
+  Future<bool> loadData() async {
+    print('Start');
+    // dummy loading
+    // creates delay of 3 seconds
+    await Future.delayed(Duration(seconds: 5));
+    _receivedData = 'Data Received';
+    print('Done');
+    return true;
   }
 }
